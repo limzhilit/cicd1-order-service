e1924bca622ecc8b3078ec7b60b97731983e41e1
limzhilit <g00440629@atu.ie>
2026-09-18 09:50:21 +0100

    initial spring boot project structure

---
+++ src/main/java/ie/atu/cicd1/order/cicd1orderservice/Cicd1OrderServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.cicd1.order.cicd1orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cicd1OrderServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Cicd1OrderServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ie/atu/cicd1/order/cicd1orderservice/Cicd1OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.cicd1.order.cicd1orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicd1OrderServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
